b3f53e73a35765187bfc6937086cbaf375a6cdd4
wqy <1950016253@qq.com>
2020-10-28 18:40:27 +0800

    首页详细布局

--- App.vue
+++ App.vue
@@ -17,9 +17,10 @@
 </script>
 
 <style>
-	/*每个页面公共css */
-	/* 引入官方样式库 */
-	@import url('/common/common.css');
-	@import url('/common/free.css');
-	@import url('/common/icon.css');
+/*引入公共样式库 */
+@import url('./common/common.css');
+/*引入free样式库 */
+@import url('./common/free.css');
+/*引入图标库 */
+@import url('./common/icon.css');
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,32 +1,96 @@
 <template>
-	<view class="content">
-		<tab></tab>
+	<view>
+
+		<!-- 轮播图 -->
+		<swiper
+			:indicator-dots="true"
+			:autoplay="true"
+			:interval="3000"
+			:duration="200"
+			style="width: 750rpx; height: 250rpx;"
+		>
+			<swiper-item>
+				<image src="../../static/banner/1.jpg" style="width: 750rpx; height: 250rpx;"></image>
+			</swiper-item>
+			<swiper-item>
+				<image src="../../static/banner/2.jpg" style="width: 750rpx; height: 250rpx;"></image>
+			</swiper-item>
+		</swiper>
+
+		<!-- 列表 -->
+		<view class="flex flex-wrap">
+			<view class="list-item" v-for="i in 10" :key="i" @click="openLive">
+				<image
+					src="/static/me.jpg"
+					style="width: 365rpx; height: 365rpx;"
+					class="rounded"
+					mode="aspectFill"
+				></image>
+
+				<view
+					class="rounded-circle px-2 flex align-center"
+					style="position: absolute; left: 15rpx; top: 15rpx; background-color: rgba(0,0,0,0.4);"
+				>
+					<text class="iconfont iconbizhongguanli text-warning mr-1">	
+					</text>
+					<text class="text-white font">0</text>
+				</view>
+				
+				<view
+					class="rounded-circle px-2 flex align-center"
+					style="position: absolute; right: 15rpx; top: 15rpx; background-color: rgba(0,0,0,0.4);"
+				>
+					<text class="font-sm text-white">
+						人气：
+					</text>
+					<text class="text-white font-sm">0</text>
+				</view>
+				
+				<view
+					class="rounded-circle flex align-center"
+					style="position: absolute; left: 15rpx; bottom: 15rpx; "
+				>
+					<text class="font text-white">
+						标题：
+					</text>
+				</view>
+				
+				<view
+					class="rounded-circle px-2 flex align-center"
+					style="position: absolute; right: 15rpx; bottom: 15rpx; background-color: rgba(0,0,0,0.4);"
+				>
+					<text style="width: 20rpx; height: 20rpx;" class="rounded-circle bg-danger mr-1">
+					</text>
+					<text class="text-white font-sm">已结束</text>
+				</view>
+				
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
-	import tab from '../../components/tab.vue'
-	export default {
-		data() {
-			return {
-			}
-		},
-		components:{
-			tab
-		},
-		onLoad() {
-			uni.hideTabBar();
-		},
-		methods: {
-
+export default {
+	data() {
+		return {};
+	},
+	onLoad() {},
+	methods: {
+		openLive() {
+			uni.navigateTo({
+				url: '../live/live'
+			});
 		}
 	}
+};
 </script>
 
 <style>
-	.content {
-		width: 100%;
-		height: 100%;
-		background: #5a5b5a;
-	}
+.list-item {
+	width: 375rpx;
+	height: 375rpx;
+	padding: 5rpx;
+	box-sizing: border-box;
+	position: relative;
+}
 </style>
